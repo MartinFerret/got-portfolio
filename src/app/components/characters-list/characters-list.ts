9469110d8ce9fa69416083fae2c5ae0d1ae412bc
MartinFerret <ferretmartin91@gmail.com>
2025-12-10 11:23:24 +0100

    initial commit.

---
+++ src/app/components/characters-list/characters-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-characters-list',
+  imports: [],
+  templateUrl: './characters-list.html',
+  styleUrl: './characters-list.scss',
+})
+export class CharactersList {
+
+}
