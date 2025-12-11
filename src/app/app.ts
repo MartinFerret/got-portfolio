fa1a10720e9fa95d46a4bb9d54f184ef074e3acb
MartinFerret <ferretmartin91@gmail.com>
2025-12-11 14:22:48 +0100

    feat: continents from api.

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,23 +3,40 @@ import { RouterOutlet } from '@angular/router';
 import {CharactersList} from './components/characters-list/characters-list';
 import {CharacterService} from './shared/services/character';
 import {Characters} from './shared/models/characters.model';
+import {ContinentsService} from './shared/services/continents-service';
+import {Continents} from './shared/models/continents.model';
+import {ContinentsList} from './components/continents-list/continents-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CharactersList],
+  imports: [RouterOutlet, CharactersList, ContinentsList],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App implements OnInit {
-  // On injecte le service précédemment créé.
+  // On injecte les services précédemment créé.
   private charactersService = inject(CharacterService);
+  private continentsService = inject(ContinentsService);
   private cdr = inject(ChangeDetectorRef);
 
   // On stock tous les personnages dans un tableau.
   protected charactersToGiveToChild!: Characters[];
+  protected continentsToGiveToChild!: Continents[];
 
   // On subscribe() uniquement sur les Observables.
   ngOnInit() {
+    this.getCharactersInTemplate();
+    this.getAllContinentsInTemplate();
+  }
+
+  private getAllContinentsInTemplate () {
+    this.continentsService.getAllContinents().subscribe((continentsFromApi: Continents[]) => {
+      this.continentsToGiveToChild = continentsFromApi;
+      this.cdr.detectChanges();
+    })
+  }
+
+  private getCharactersInTemplate () {
     this.charactersService.getCharacters().subscribe((charactersFromApi: Characters[]) => {
       this.charactersToGiveToChild = charactersFromApi;
       this.cdr.detectChanges();
