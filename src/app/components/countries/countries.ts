f8d6028b596c84b55364a3cf3efb240b9d57d8bc
MartinFerret <ferretmartin91@gmail.com>
2025-12-12 16:49:03 +0100

    feat: routes.

---
+++ src/app/components/countries/countries.ts
@@ -0,0 +1,34 @@
+import {Component, inject, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Title} from '@angular/platform-browser';
+import {CountriesModel} from '../../shared/models/countries.model';
+
+@Component({
+  selector: 'app-countries',
+  imports: [],
+  templateUrl: './countries.html',
+  styleUrl: './countries.scss',
+})
+export class Countries implements OnInit {
+  protected currentTitle = '';
+  protected countries: CountriesModel[] = [];
+
+  private activatedRoute = inject(ActivatedRoute);
+  private titleService = inject(Title);
+
+  ngOnInit() {
+    this.getCountries();
+    this.currentTitle = this.titleService.getTitle();
+  }
+
+  protected changeTitle() {
+    this.titleService.setTitle('Pays');
+    this.currentTitle = this.titleService.getTitle();
+  }
+
+  private getCountries() {
+    this.activatedRoute.data.subscribe((data) => {
+      this.countries = data['countries'];
+    })
+  }
+}
---
+++ src/app/components/home/home.ts
@@ -0,0 +1,56 @@
+import {ChangeDetectorRef, Component, inject, OnInit} from '@angular/core';
+import {Characters} from '../../shared/models/characters.model';
+import {Continents} from '../../shared/models/continents.model';
+import {ContinentsService} from '../../shared/services/continents-service';
+import {CharacterService} from '../../shared/services/character';
+import {CharactersList} from '../characters-list/characters-list';
+import {ContinentsList} from '../continents-list/continents-list';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    CharactersList,
+    ContinentsList,
+  ],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home implements OnInit {
+// On injecte les services précédemment créé.
+  private charactersService = inject(CharacterService);
+  private continentsService = inject(ContinentsService);
+  private cdr = inject(ChangeDetectorRef);
+
+  // On stock tous les personnages dans un tableau.
+  protected charactersToGiveToChild!: Characters[];
+  protected continentsToGiveToChild!: Continents[];
+  protected filteredCharacters!: Characters[];
+
+  // On subscribe() uniquement sur les Observables.
+  ngOnInit() {
+    this.getCharactersInTemplate();
+    this.getAllContinentsInTemplate();
+  }
+
+  protected onSearch(term: string) : void {
+    this.filteredCharacters = this.charactersToGiveToChild.filter((character: Characters) => {
+      const fullName = character.fullName ?? '';
+      return fullName.toLowerCase().includes(term.toLowerCase())
+    })
+  }
+
+  private getAllContinentsInTemplate () {
+    this.continentsService.getAllContinents().subscribe((continentsFromApi: Continents[]) => {
+      this.continentsToGiveToChild = continentsFromApi;
+      this.cdr.detectChanges();
+    })
+  }
+
+  private getCharactersInTemplate () {
+    this.charactersService.getCharacters().subscribe((charactersFromApi: Characters[]) => {
+      this.charactersToGiveToChild = charactersFromApi;
+      this.filteredCharacters = charactersFromApi;
+      this.cdr.detectChanges();
+    })
+  }
+}
