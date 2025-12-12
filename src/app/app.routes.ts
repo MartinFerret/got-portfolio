9498448f2cfeb2006135627d5ede1def4e6f32d3
MartinFerret <ferretmartin91@gmail.com>
2025-12-12 16:49:28 +0100

    feat: routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import {Home} from './components/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home }, // Eager loading - Pour les pages présentes directement, comme une homepage.
+  { path: 'home', redirectTo: '', pathMatch: 'full'}, // Redirection.
+  { path: 'countries',
+    loadComponent: () => import('./components/countries/countries') // Lazy-loading - Charger dynamiquement le composant. Si la route n'est pas activée, le composant n'est pas chargé.
+      .then((component) => component.Countries),
+    title: 'Countries',
+    data: {
+    countries: [
+      {id: 1, name: 'France'},
+      {id: 2, name: 'USA'},
+      {id: 3, name: 'Germany'},
+      {id: 4, name: 'Spain'}
+    ]
+    }
+  }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,55 +1,10 @@
-import {ChangeDetectorRef, Component, inject, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {CharactersList} from './components/characters-list/characters-list';
-import {CharacterService} from './shared/services/character';
-import {Characters} from './shared/models/characters.model';
-import {ContinentsService} from './shared/services/continents-service';
-import {Continents} from './shared/models/continents.model';
-import {ContinentsList} from './components/continents-list/continents-list';
-import {NgClass, NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CharactersList, ContinentsList, NgClass, NgStyle],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App implements OnInit {
-  // On injecte les services précédemment créé.
-  private charactersService = inject(CharacterService);
-  private continentsService = inject(ContinentsService);
-  private cdr = inject(ChangeDetectorRef);
-
-  // On stock tous les personnages dans un tableau.
-  protected charactersToGiveToChild!: Characters[];
-  protected continentsToGiveToChild!: Continents[];
-  protected filteredCharacters!: Characters[];
-
-  // On subscribe() uniquement sur les Observables.
-  ngOnInit() {
-    this.getCharactersInTemplate();
-    this.getAllContinentsInTemplate();
-  }
-
-  protected onSearch(term: string) : void {
-    this.filteredCharacters = this.charactersToGiveToChild.filter((character: Characters) => {
-      const fullName = character.fullName ?? '';
-      return fullName.toLowerCase().includes(term.toLowerCase())
-    })
-  }
-
-  private getAllContinentsInTemplate () {
-    this.continentsService.getAllContinents().subscribe((continentsFromApi: Continents[]) => {
-      this.continentsToGiveToChild = continentsFromApi;
-      this.cdr.detectChanges();
-    })
-  }
-
-  private getCharactersInTemplate () {
-    this.charactersService.getCharacters().subscribe((charactersFromApi: Characters[]) => {
-      this.charactersToGiveToChild = charactersFromApi;
-      this.filteredCharacters = charactersFromApi;
-      this.cdr.detectChanges();
-    })
-  }
-}
+export class App {}
